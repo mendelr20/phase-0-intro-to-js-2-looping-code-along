8d0e448a63cd6e0cad7e882034448260870a88d8
Menachem Rosenblum <mendel@rosencrew.com>
2022-01-10 12:29:54 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+function writeCards(names, event) {
+  let newArray = [] 
+  for (let i = 0; i < names.length; i++){
+  newArray.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return newArray
+}
+
+
+
+
+
+function countDown(num) {
+  let i = num;
+  while (i >=0) {
+    console.log(i--);
+  }
+}
+countDown(10);
